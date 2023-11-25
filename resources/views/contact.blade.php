177f1f46ae228708550ae0a3ecbb10149149d080
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2023-11-25 23:15:22 +0100

    Add views for simple routes

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Beyond Borders</title>
+
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+    <x-Header />
+
+    <main>
+
+        <x-Navigation />
+
+        <section class="featured-articles">
+            <h1>Contact-View<h1>
+        </section>
+
+    </main>
+
+    <x-Footer />
+</body>
+
+</html>
