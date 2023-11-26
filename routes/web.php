6f1d0bd7b347a4eb2935490f77c9cac0733ed956
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2023-11-26 13:06:40 +0100

    Adjust routes for users & login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\HTTP\Controllers\UserController;
+
+use function PHPUnit\Framework\returnSelf;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +21,31 @@ Route::view('/home', 'home');
 Route::view('/about', 'about');
 Route::view('/destinations', 'destinations');
 Route::view('/advice', 'advice');
+
+Route::post('/login', [UserController::class, 'login']);
+
+Route::get('/login', function () {
+    if (session()->has('username'))
+        return redirect('/user/{name}')->with('name', session()->get('username'));
+
+    return view('login');
+});
+
+Route::get('/logout', function () {
+    session()->invalidate();
+
+    return redirect('/home');
+});
+
+Route::get('/user/{name?}', function (?string $name = null) {
+    if (!session()->has('username'))
+        return redirect('/login');
+
+    if (!$name)
+        return redirect('/user' . '/' . session()->get('username'));
+
+    return view('user');
+});
 
 Route::view('/blog', 'blog');
 Route::view('/contact', 'contact');
