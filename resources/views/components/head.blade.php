04e9811083b38dd51f270952b85d146a744a0b14
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2023-11-28 05:40:38 +0100

    Create Component Head

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Beyond Borders</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&family=Kaushan+Script&family=Kavivanar&family=Macondo&family=Over+the+Rainbow&family=Rock+Salt&family=Julee&family=Vujahday+Script&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+</head>
